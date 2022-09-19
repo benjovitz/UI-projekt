df9edb6d3b075142879ba2b9720b95a823f994e0
benjovitz <dbenjovitz@gmail.com>
2022-09-19 15:28:19 +0200

    shared

--- src/main/java/com/example/uiprojekt/controller/controller.java
+++ src/main/java/com/example/uiprojekt/controller/controller.java
@@ -1,4 +1,15 @@
 package com.example.uiprojekt.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class controller {
+
+    @GetMapping("/")
+    public String frontPage(){
+        return "testpage";
+    }
 }
+
+
