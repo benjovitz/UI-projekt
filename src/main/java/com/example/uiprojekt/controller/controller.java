8f6414397f4e9b6de238d46aedf99d32aa47657f
benjovitz <dbenjovitz@gmail.com>
2022-09-19 14:34:15 +0200

    first commit

---
+++ src/main/java/com/example/uiprojekt/controller/controller.java
@@ -0,0 +1,4 @@
+package com.example.uiprojekt.controller;
+
+public class controller {
+}
---
+++ src/test/java/com/example/uiprojekt/UiProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uiprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
