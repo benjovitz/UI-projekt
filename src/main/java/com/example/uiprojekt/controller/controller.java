da00facd9bcdcd15108efc87df7593edce75c563
benjovitz <dbenjovitz@gmail.com>
2022-09-22 11:38:31 +0200

    daniel

--- src/main/java/com/example/uiprojekt/controller/controller.java
+++ src/main/java/com/example/uiprojekt/controller/controller.java
@@ -2,14 +2,27 @@ package com.example.uiprojekt.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class controller {
 
+    private ArrayList<String> list;
+
+    public controller(){
+        list = new ArrayList<>();
+
+        list.add("white.png");
+        list.add("black.png");
+    }
+
     @GetMapping("/")
     public String frontPage(){
-        return "testpage";
+        return "merch";
     }
+
 }
 
 
