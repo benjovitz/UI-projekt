8ac0cbc3ece51a0b11af4deb37ff113976610b22
LasseDall <lass794rrstud.kea.dk>
2022-09-22 12:31:29 +0200

    Merge commit 2

--- src/main/java/com/example/uiprojekt/controller/controller.java
+++ src/main/java/com/example/uiprojekt/controller/controller.java
@@ -1,11 +1,17 @@
 package com.example.uiprojekt.controller;
 
+import com.example.uiprojekt.service.UIService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class controller {
+
+    UIService uiService = new UIService();
 
     @GetMapping("/")
     public String frontPage(){
@@ -40,6 +46,22 @@ public class controller {
     @GetMapping("/tricks")
     public String createTricksSite() {
         return "tricks";
+    }
+
+    @PostMapping("/frontpage-co2")
+    public String co2FrontPost(@RequestParam("madvare") String food, @RequestParam("kg") Integer kg, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addAttribute("madvare", food);
+        redirectAttributes.addAttribute("kg", kg);
+        return "redirect:/frontpage-co2";
+    }
+
+    @GetMapping("/frontpage-co2")
+    public String co2Front(@RequestParam("madvare") String food, @RequestParam("kg") Integer kg, Model model) {
+        model.addAttribute("madvare", food);
+        model.addAttribute("kg", kg);
+        double co2 = uiService.calculateCO2(food,kg);
+        model.addAttribute("co2", co2);
+        return "frontpage-co2";
     }
 }
 
