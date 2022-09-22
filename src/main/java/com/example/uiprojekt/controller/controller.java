b776c533fbe013d81cb552c9772e754102127b88
LasseDall <lass794rrstud.kea.dk>
2022-09-22 11:36:03 +0200

    Merge commit

--- src/main/java/com/example/uiprojekt/controller/controller.java
+++ src/main/java/com/example/uiprojekt/controller/controller.java
@@ -2,13 +2,44 @@ package com.example.uiprojekt.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class controller {
 
     @GetMapping("/")
     public String frontPage(){
-        return "testpage";
+        return "frontpage";
+    }
+
+    @GetMapping("/home")
+    public String homeFrontPage(){
+        return "frontpage";
+    }
+
+    @PostMapping("/home")
+    public String createFrontPage() {
+        return "frontpage";
+    }
+
+    @PostMapping("/merch")
+    public String merch(){
+        return "merch";
+    }
+
+    @GetMapping("/merch")
+    public String createMerchSite() {
+        return "merch";
+    }
+
+    @PostMapping("/tricks")
+    public String tricks(){
+        return "tricks";
+    }
+
+    @GetMapping("/tricks")
+    public String createTricksSite() {
+        return "tricks";
     }
 }
 
---
+++ src/main/java/com/example/uiprojekt/service/UIService.java
@@ -0,0 +1,12 @@
+package com.example.uiprojekt.service;
+
+import java.util.HashMap;
+
+public class UIService {
+
+  private HashMap<String, Integer> foodMap;
+
+  public UIService() {
+    foodMap = new HashMap<>();
+  }
+}
